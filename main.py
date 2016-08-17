0e20e490f56ad1720e15d5bedc41b9917442c757
Albert-Tate <albert.c.tate@gmail.com>
2016-08-17 14:56:09 +1000

    Got it forming KCL equations rather blindly. Will have to think about how to intelligently handle voltage sources as we go forward

--- main.py
+++ main.py
@@ -1,6 +1,59 @@
 #!/usr/bin/env python
 import sys
 
+"""
+#Define current directions
+#e.g. IR1 from node A -> B
+if '0' not in nodeNames:
+	print "No Ground, exiting..."
+	sys.exit()
+
+print "\nFile read successfully!\n"
+print "Defining Current Directions...\n"
+
+for res in rlist:
+	print res
+	print 'I' + rNames[rlist.index(res)] + \
+	" from node " + nodeNames[res[0]] + \
+	" to node " + nodeNames[res[1]] 
+for v in vlist:
+	print v
+	print 'I' + vsNames[vlist.index(v)] + \
+	" from node " + nodeNames[v[0]] + \
+	" to node " + nodeNames[res[1]]
+
+Iin = []
+Iout = []
+#Form KCL equations based on this
+for ind,node in enumerate(nodeNames):
+	if node == '0':
+		continue
+	print "\n\n@ Node: " + node
+	#Find currents
+	for res in rlist:
+		if res[1] == ind:
+			#res describes input current
+			Iin.append('I' + rNames[rlist.index(res)])
+		elif res[0] == ind:
+			#res describes output current
+			Iout.append('I' + rNames[rlist.index(res)])
+	for v in vlist:
+		if v[1] == ind:
+			#Iv describes input current	
+			Iin.append('I' + vsNames[vlist.index(v)])
+		elif v[0] == ind:
+			Iout.append('I' + vsNames[vlist.index(v)])
+	if len(Iin) == 0:
+		Iin = '0'
+	if len(Iout) == 0:
+		Iout = '0'	
+	print " + ".join(Iin) + " = " + " + ".join(Iout)
+	
+	Iin = []
+	Iout = [
+"""
+
+
 class CircuitSolver:
 
 	def __init__(self):
@@ -18,6 +71,74 @@ class CircuitSolver:
 		self.vList = []
 		self.iList = []
 		self.rList = []
+
+		#KCL item index correlates to nodeName index
+		#[ [(fromNode, toNode, name), (fromNode, toNode)], [(fromNode, toNode)] ]
+		#Each index contains two arrays: Iin and Iout
+		#Each Iin/Iout entry is described as a tuple (fromNode, toNode)
+		self.KCL = []
+
+	def make_kcl(self):
+		if '0' not in self.nodeNames:
+			print "No ground found, exiting..."
+			sys.exit()
+		for ind, node in enumerate(self.nodeNames):
+			temp = [ [], [] ]
+			if node == '0':
+				self.KCL.append(temp)
+				continue
+			for rind, res in enumerate(self.rList):
+				if res[1] == ind:
+				  #res describes input
+				  temp[0].append((res[0],res[1],self.rNames[rind]))
+				elif res[0] == ind:
+				  temp[1].append((res[0],res[1],self.rNames[rind]))
+			for vind, v in enumerate(self.vList):
+				if v[1] == ind:
+				  temp[0].append((v[0], v[1], self.vNames[vind]))
+				elif v[0] == ind:
+				  temp[1].append((v[0],v[1],self.vNames[vind]))
+			self.KCL.append(temp)
+			#TODO: Add support for vsource/Isource
+
+	def print_kcl(self, resBool):
+		for ind, eqn in enumerate(self.KCL):
+			sTemp = "\t";
+			if self.nodeNames[ind] == '0':
+				continue
+			print "@ " + self.nodeNames[ind]
+			if eqn[0] == []:
+				sTemp += "0"
+			else:
+				for i in eqn[0]:
+					if not resBool: 
+					  sTemp += "I" + i[2] + " + "
+					else:
+					  sTemp += "(" + \
+						self.nodeNames[i[0]] + " - " + \
+						self.nodeNames[i[1]] + ")/" + \
+						i[2] + " + "
+
+				sTemp = sTemp[0:-3]
+			sTemp += " = "	
+			if eqn[1] == []:
+				sTemp += "0"
+			else:
+				for i in eqn[1]:
+					if not resBool: 
+					  sTemp += "I" + i[2] + " + "
+					else:
+					  sTemp += "(" + \
+						self.nodeNames[i[0]] + " - " + \
+						self.nodeNames[i[1]] + ")/" + \
+						i[2] + " + "
+				sTemp = sTemp[0:-3]
+			
+			if "V1" not in sTemp:
+				print sTemp
+			else:
+				print "Voltage Source Prevents Nodal here"
+
 
 	def __parse_line(self, line, component):
 
@@ -64,76 +185,17 @@ new = CircuitSolver()
 
 new.read_file('spiceneteg.net')
 
-print new.nodeNames
+
+"""print new.nodeNames
 print new.vNames
 print new.rNames
 print new.rList
 print new.vList
-	
 """
-#Globals. Complain about it, see if I care
-nodeNames = [] #Text repr of nodes, voltages etc in these vars
-vsNames = []
-isNames = []
-rNames = []
+new.make_kcl()
 
-vlist = [] #[[from, to, val]] Where from and to are indexes in nodeNames
-	   #and index of vlist is index of vsNames
-ilist = []
-rlist = []
-
-
-nCount = 0
-rCount = 0
-
-def parse_netfile_part(line, t_part):
-	global nodeNames; global vsNames; global isNames; global rNames;
-	global vlist; global ilist; global nCount; global rCount
-
-	temp = line.split();
-	if len(temp) != 4:
-		print "Length of line too long, aborting\n"
-		sys.exit()
-	if temp[1] in nodeNames:
-		start = nodeNames.index(temp[1])
-	else:
-		nodeNames.append(temp[1])
-		start = nCount; 
-		nCount+=1
-	if temp[2] in nodeNames:
-		end = nodeNames.index(temp[2])
-	else:
-		nodeNames.append(temp[2])
-		end = nCount
-		nCount+=1
-
-	if t_part == 'R':	
-		rNames.append(temp[0]); rCount+=1
-		rlist.append([start, end, int(temp[3])])
-	elif t_part == 'V':
-		vsNames.append(temp[0]);
-		vlist.append([start, end, int(temp[3])])
-	elif t_part == 'I':
-		isNames.append(temp[0]);
-		ilist.append([start, end, int(temp[3])])
-
-
-#File reader for netfiles. Loads up globals
-def parse_netfile(fhandle):
- global nodeNames; global vsNames; global isNames; global rNames;
- global vlist; global ilist; global nCount; global rCount
-
- for line in fhandle:
-	if line[0] == 'R':
-		parse_netfile_part(line, 'R');
-	elif line[0] == 'V':
-		parse_netfile_part(line, 'V');	
-	elif line[0] == 'I':
-		parse_netfile_part(line, 'I');
-	else:
-		print "IGNORED:\n\t", line
-
-"""
+new.print_kcl(0)
+new.print_kcl(1)
 """
 * C:\Program Files (x86)\LTC\LTspiceIV\Draft1.asc
 V1 N001 N004 4
